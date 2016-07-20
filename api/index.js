13b46ebe36bb0df947fc6bd4be21717f0c8e1173
Chris Occhipinti <cocchipinti@outbrain.com>
2016-07-20 16:00:36 -0400

    added api endpoint for getting performance by day for a campaign

--- api/index.js
+++ api/index.js
@@ -1,15 +1,51 @@
 var requestHelper = require('../utils/request');
 
+/**
+ * @function getMarketers Gets a list of marketers
+ * @returns {object} Returns a promise.
+ * @public
+ */
 module.exports.getMarketers = function getMarketers() {
   return requestHelper({url: '/marketers'});
 };
 
+/**
+ * @function getBudgets Gets a list of budgets
+ * @param {string} marketerId The marketer ID to get budgets for
+ * @returns {object} Returns a promise.
+ * @public
+ */
 module.exports.getBudgets = function(marketerId) {
   return requestHelper({url: `/marketers/${marketerId}/budgets`});
 };
 
+/**
+ * @function getCampaigns Gets a list of campaigns
+ * @param {string} marketerId The marketer ID to get campaigns for
+ * @param {boolean=} includeArchived Whether or not to return archived campaigns (default: false)
+ * @param {string} fetch Indicates the size of the payload to return, can be either 'basic' or 'all' (default: 'all') 
+ * @returns {object} Returns a promise.
+ * @public
+ */
 module.exports.getCampaigns = function(marketerId, includeArchived, fetch) {
   includeArchived = includeArchived || false;
   fetch = fetch || 'all';
-  return requestHelper({url: `/marketers/${marketerId}/campaigns?includeArchived=${includeArchived}&fetch=`, qs: {includeArchived, fetch}});
+  return requestHelper({url: `/marketers/${marketerId}/campaigns`, qs: {includeArchived, fetch}});
+};
+
+/**
+ * @function getPerformanceByDay
+ * @param {string} campaignId The campaign ID to get performance metrics for
+ * @param {object} params
+ * @param {string} params.from The start date (format: YYYY-MM-DD)
+ * @param {string} params.to The end date (format: YYYY-MM-DD)
+ * @param {number=} params.limit The number of results to return (default: 10)
+ * @param {string=} params.sort 'ctr', 'impressions', 'cost', 'clicks', 'date', prepended with a '+' (descending) or a '-' (ascending) (default: '+date')
+ * @returns {object} Returns a promise.
+ * @public
+ */
+module.exports.getPerformanceByDay = function getPerformanceByDay(campaignId, params) {
+  params.limit = params.limit || 10;
+  params.sort = params.sort || '+date';
+  return requestHelper({url: `/campaigns/${campaignId}/performanceByDay`, qs: params});
 };
